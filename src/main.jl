a45ff7be5603ffb7a4b2b59cab44b7e4195c558d
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-09-11 12:09:57 -0300

    Add pack_color and unpack_color functions

--- src/main.jl
+++ src/main.jl
@@ -15,3 +15,14 @@ A = fill!(A,0.0)
 plot(A)
 
 
+function pack_color(const r::UInt8, const g::UInt8, const b::UInt8, const a::UInt8 = 255)
+    return (a<<24) + (b<<16) + (g<<8) + r;
+end
+
+function unpack_color(const color::UInt32)
+    r::UInt8 = (color >> 0) & 255
+    g::UInt8 = (color >> 8) & 255
+    b::UInt8 = (color >> 16) & 255
+    a::UInt8 = (color >> 24) & 255
+    return r, g, b, a
+end
