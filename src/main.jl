bd4dc1ff65bbfde34c1334c831649154d91c2b4f
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-09-11 20:01:27 -0300

    Add create_gradient and drop_ppm_image functions

--- src/main.jl
+++ src/main.jl
@@ -21,12 +21,42 @@ function unpack_color( color::UInt32)
     return r, g, b, a
 end
 
+function drop_ppm_image( filename::String, image::Array{UInt32,1}, width, height)
+    file = open(filename, "w")
+    print(file, "P3\n$(width)\t$(height)\n255\n")
+    for i = 1:width*height
+        r, g, b, a = unpack_color(image[i])
+        print(file, "$r $g $b ")
+
+    end
+    close(file)
+end
+
+function create_gradient(A::Array{UInt32,1}, width, height)
+    for j = 1:height
+        for i = 1:width
+            r::UInt8 = floor(255*j/height)
+            g::UInt8 = floor(255*i/width)
+            b::UInt8 = 0 
+            A[i+(j-1)*width] = pack_color(r, g, b)
+        end
+    end
+    return A
+end
+ 
 function main()
     # Defining canvas size
     width = 400
     height = 300
         
-    # Creating canvas array
+    # Creating canvas array, initialized to red
     A = Array{UInt32,1}(undef, height * width)
     A = fill!(A, 255)
+    
+    # Creating gradient
+    A = create_gradient(A, width, height)
+    
+    # Saving image to .ppm format
+    drop_ppm_image("./out.ppm", A, width,  height)
+    println("Done")
 end
