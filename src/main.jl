e8e02d693f1ce4c60ecfe9dd0f7a43e7a86ae233
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2020-09-11 15:45:15 -0300

    Add main function fix pack+unpack functions

--- src/main.jl
+++ src/main.jl
@@ -2,27 +2,31 @@
 using Colors
 using Plots
 
-# Defining canvas size
-width = 400
-height = 300
 
 
-# Creating canvas array
-A = Array{RGB{},2}(undef, height , width)
-A = fill!(A,0.0)
 
 # Plotting array
-plot(A)
+#plot(A)
 
 
-function pack_color(const r::UInt8, const g::UInt8, const b::UInt8, const a::UInt8 = 255)
-    return (a<<24) + (b<<16) + (g<<8) + r;
+function pack_color(r::UInt8,  g::UInt8, b::UInt8, a::UInt8 = 0xff)
+    return (UInt32(a)<<24) + (UInt32(b)<<16) + (UInt32(g)<<8) + r;
 end
 
-function unpack_color(const color::UInt32)
+function unpack_color( color::UInt32)
     r::UInt8 = (color >> 0) & 255
     g::UInt8 = (color >> 8) & 255
     b::UInt8 = (color >> 16) & 255
     a::UInt8 = (color >> 24) & 255
     return r, g, b, a
+end
+
+function main()
+    # Defining canvas size
+    width = 400
+    height = 300
+        
+    # Creating canvas array
+    A = Array{UInt32,1}(undef, height * width)
+    A = fill!(A, 255)
 end
